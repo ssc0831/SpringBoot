4d0c6d8d21e64b82ce4adac5cce02c059ff417ff
ssc0831 <ssc0831@naver.com>
2023-09-11 15:07:10 +0900

    React Spring Boot(JPA 2)

--- app09_todo/src/main/java/com/example/todo/repository/ToDoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.todo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.todo.model.ToDo;
-
-public interface ToDoRepository extends JpaRepository<ToDo, Long> {
-
-}
