3f4745590c947b8dfe212997ec7293ad01579004
ssc0831 <ssc0831@naver.com>
2023-09-15 16:05:26 +0900

    React

--- app111_movie/src/main/java/com/example/app111_movie/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app111_movie.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.app111_movie.model.Movie;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-
-}
